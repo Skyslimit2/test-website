134a27e8257f5e2f4c6fbb25517fa9f15b2a7805
Skyslimit2 <snakesupplier@gmail.com>
2025-10-01 17:36:48 -0400

    ege

---
+++ sketch.js
@@ -0,0 +1,38 @@
+let ege;
+
+function preload() {
+  ege = loadImage('ege.gif'); 
+}
+
+
+function setup() {
+  let canvasWidth = min(windowWidth, windowHeight * 0.5625);
+  let canvasHeight = canvasWidth * 1.777; 
+
+
+  if (canvasHeight > windowHeight) {
+        canvasHeight = windowHeight;
+        canvasWidth = canvasHeight * 0.5625;
+  }
+
+  createCanvas(canvasWidth, canvasHeight);
+
+  //console.log(ege);
+  background(50);
+}
+
+function draw() {
+  background(10, 10, 10);
+
+  let scaleX = width / ege.width;
+  let scaleY = height / ege.height;
+  let scale = max(scaleX, scaleY);
+
+
+  let scaledWidth = ege.width * scale;
+  let scaledHeight = ege.height * scale;
+  let x = (width - scaledWidth) / 2;
+  let y = (height - scaledHeight) / 2;
+
+  image(ege, x, y, scaledWidth, scaledHeight);
+}
